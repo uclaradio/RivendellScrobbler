e2d6421dfd0294701f6616b2cd1186bc812eb4f7
chrislaganiere <chrislaganiere@gmail.com>
2017-01-22 04:27:48 +0000

    working scrobbles

--- app.js
+++ app.js
@@ -9,34 +9,63 @@ var lfm = new LastfmAPI({
     'secret' : passwords.lastfm_secret
 });
 
-function authentication(callback) {
+var scrobbleHistoryLocation = "./scrobbles.txt";
+
+function authenticateSession(callback) {
 	lfm.auth.getMobileSession(passwords.lastfm_user, passwords.lastfm_password, function(err, session) {
 		lfm.setSessionCredentials(session.username, session.key);
 		callback();
 	});
 }
 
-function scrobble(artist, track) {
+function scrobble(artist, track, timestamp) {
 	lfm.track.scrobble({
 	    'artist' : artist,
 	    'track' : track,
-	    'timestamp' : Math.floor((new Date()).getTime() / 1000)
+	    'timestamp' : timestamp
 	}, function (err, scrobbles) {
 	    if (err) { return console.log('Failed to scrobble!', artist, track, ":", err); }
 	    console.log('Scrobbled:', scrobbles);
 	});
 }
 
-lfm.auth.getMobileSession(passwords.lastfm_user, passwords.lastfm_password, function(err, session) {
-	console.log(err);
-	console.log(session);
-	console.log("finished.");
-	lfm.setSessionCredentials(session.username, session.key);
+function readLines(filePath, callback) {
+	var lineReader = require('readline').createInterface({
+	  input: require('fs').createReadStream(filePath)
+	});
 
-	
+	lineReader.on('line', function (line) {
+	  callback(line);
+	});
+}
+
+var lastTimestamp = null;
+function loadSongHistory(filePath, newSongCallback) {
+	readLines(filePath, function(line) {
+		var pattern = /([^\n]*)---([^\n]*)---([^\n]*)/g;
+		var match = pattern.exec(line);
+		if (match) {
+			var artist = match[1];
+			if (artist) {
+				artist = artist.trim();
+			}
+			var track = match[2];
+			if (track) {
+				track = track.trim();
+			}
+			var timestamp = Math.floor(Date.parse(match[3]) / 1000) ;
+			if (!lastTimestamp || lastTimestamp < timestamp) {
+				// found a new track!
+				newSongCallback(artist, track, timestamp);
+				lastTimestamp = timestamp;
+			}
+		}
+	});
+}
+
+var file = process.argv[2];
+
+authenticateSession(function() {
+	loadSongHistory(file, scrobble);
 });
-
-
-
-
 
